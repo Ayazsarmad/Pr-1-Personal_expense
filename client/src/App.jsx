9752914b6ba1d49a862b884ef3f725cc829717d2
Ayazsarmad <m.ayazsarmad@gmail.com>
2024-06-17 03:03:03 -0700

    label section done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,28 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
-import Graph from './components/Graph'
+import "./App.css";
+import Graph from "./components/Graph";
+import Form from "./components/Form";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className='App'>
+    <div className="App">
       <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800 ">
-      <h1 className='text-4xl py-5 mb-10 bg-slate-800 text-white rounded'>Expense Tracker</h1>
+        <h1 className="text-4xl py-5 mb-10 bg-slate-800 text-white rounded">
+          Expense Tracker
+        </h1>
 
-      <div className="grid md:grid-cols-2 gap-4">
-        {/* chart */}
-        <Graph/>
-        {/* form */}
+        <div className="grid md:grid-cols-2 gap-4">
+          {/* chart */}
+          <Graph />
+          {/* form */}
+          <Form />
+        </div>
       </div>
-      </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/Form.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Form = () => {
+  return <div>form my</div>;
+};
+
+export default Form;
